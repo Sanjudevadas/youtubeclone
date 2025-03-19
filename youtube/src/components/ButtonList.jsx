9b4a139b796258d3f727c36b379de5c36f9e1ff1
Sanju Devadas <sanjudevadas11@gmail.com>
2025-03-19 10:46:30 +0530

    creating video list

--- youtube/src/components/ButtonList.jsx
+++ youtube/src/components/ButtonList.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const ButtonList = () => {
-  return (
-    <div>ButtonList</div>
-  )
-}
+  const buttons = [
+    "All",
+    "Music",
+    "Songs",
+    "Live",
+    "Football",
+    "Cooking",
+    "Valentine's",
+    "Movies",
+    "News",
+    "Drama",
+  ];
 
-export default ButtonList
+  return (
+    <div className="flex overflow-x-auto whitespace-nowrap scrollbar-hide p-2">
+      {buttons.map((btn, index) => (
+        <Button key={index} text={btn} />
+      ))}
+    </div>
+  );
+};
+
+export default ButtonList;
