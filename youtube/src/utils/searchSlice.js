1ca958d1182c6abca3316c7d4406963645d22543
Sanju Devadas <sanjudevadas11@gmail.com>
2025-03-20 15:21:01 +0530

    suggetions

---
+++ youtube/src/utils/searchSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+import reducer from "./appSlice";
+
+const searchSlice = createSlice({
+
+    name:'search',
+    initialState:{
+
+    },
+    reducers:{
+
+    }
+})
